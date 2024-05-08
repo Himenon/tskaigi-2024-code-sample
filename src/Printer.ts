a0fad188c0708ed46c61eb2cb5c840dd24b4ae12
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-05-08 15:47:30 +0900

    chore: update

--- src/Printer.ts
+++ src/Printer.ts
@@ -6,14 +6,25 @@ export type TransformerFactory<T extends ts.Node> = ts.TransformerFactory<T>;
  * Traverse処理なし
  */
 export const generateWithoutTransformer = (sourceText: string): string => {
+  // ==========
+  // 抽象木の作成
+  // ==========
   const source = ts.createSourceFile(
     "sample.ts",
     sourceText,
     ts.ScriptTarget.ESNext
   );
+
+  // ==============
+  // Transformation
+  // ==============
   const transformers: TransformerFactory<ts.SourceFile>[] = [];
   const result = ts.transform(source, transformers);
   result.dispose();
+
+  // ==============
+  // Code Generate
+  // ==============
   const printer = ts.createPrinter();
   const transformedSourceFile = result.transformed[0];
   return printer.printFile(transformedSourceFile);
