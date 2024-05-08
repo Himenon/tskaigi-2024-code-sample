d8b369fc565422e25cbd4d1ed8daf6de2d45cfbd
K.Himeno <6715229+Himenon@users.noreply.github.com>
2024-05-08 14:36:24 +0900

    chore: update

--- README.md
+++ README.md
@@ -1,9 +1,5 @@
-# @himenon/template-esm-js
+# Code Generator Template
 
-## Release
-
-- Automatic version updates are performed when merged into the `main` branch.
-
-## LICENCE
-
-[@Himenon/template-esm-js](https://github.com/Himenon/template-esm-js)・MIT
+```bash
+pnpm run start
+```
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,1 @@
-export * as Hello from "./hello/world.js";
-export * as Sub from "./sub.js";
-
-export const hello = (name: string): string => {
-  const params = {
-    hoge: 1,
-    fuga: 2,
-  };
-  return `Hello ${name} ${JSON.stringify(params)}`;
-};
-
-console.log(hello("Your name"));
-
-export default hello;
+console.log("HELLO");
